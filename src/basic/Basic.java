3cd0dd655c769439ad3521148559131af2a48195
bharat <balu.bharat39@gmail.com>
2021-07-30 15:58:18 +0530

    newly modified in ist

--- src/basic/Basic.java
+++ src/basic/Basic.java
@@ -5,9 +5,8 @@ public class Basic {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-		System.out.println("How");
-		System.out.println("are");
-		System.out.println("you?");
+		System.out.println("bharat loves chandhini");
+		
 
 	}
 
